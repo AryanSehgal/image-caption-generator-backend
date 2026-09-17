3bbff06733e41ea45117bf6b4e78017e63363cbf
AryanSehgal <59551957+AryanSehgal@users.noreply.github.com>
2026-09-17 14:49:06 +0530

    Update caption_gen.py

--- caption_gen.py
+++ caption_gen.py
@@ -5,10 +5,14 @@ from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import gc
 
 model = load_model('weights/model_9.h5')
 model_temp = ResNet50(weights='imagenet',input_shape=(224,224,3))
 model_resnet = Model(model_temp.input,model_temp.layers[-2].output)
+
+del model_temp
+gc.collect()
 
 def preprocess_image(img):
     print(img,file=sys.stderr)
